4257a7c606a4ef0da8939577c78edf02c0c9b2f4
Timothy-py <adeyeyetimothy33@gmail.com>
2024-09-22 21:53:25 +0100

    ft<User>:get users with borrowed books API

--- AdminService/app/schemas/user_schema.py
+++ AdminService/app/schemas/user_schema.py
@@ -20,3 +20,26 @@ class PaginatedUserResponseSchema(BaseModel):
     users: List[UserSchema]
     page: int
     size: int
+
+
+class BorrowedBookSchema(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    book_universal_id: str
+    borrower_universal_id: str
+    borrowed_date: date
+    return_date: date
+    returned: bool
+    book: dict
+
+class UserWithBorrowedBooksSchema(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    universal_id: str
+    email: str
+    first_name: str
+    last_name: str
+    borrowed_books: List[BorrowedBookSchema]
+
+class PaginatedUsersResponse(BaseModel):
+    users: List[UserWithBorrowedBooksSchema]
+    page: int
+    size: int
